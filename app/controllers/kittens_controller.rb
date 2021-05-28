19fa44c1e226d0be30f4f98417c469d0e2667791
williambayliss <wmbayliss@gmail.com>
2021-05-27 22:08:20 -0400

    Add kittens controller

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,47 @@
+class KittensController < ApplicationController
+   def index
+    @kittens = Kitten.all 
+   end
+   
+   def show
+    @kitten = Kitten.find(params[:id])
+   end
+
+   def new
+    @kitten = Kitten.new
+   end
+
+   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to kitten_path(@kitten), notice: "Kitten saved!"
+    else
+      flash.now[:alert] = "You fucked it up!"
+      render 'new'
+    end
+   end
+
+   def edit
+    @kitten = Kitten.find(params[:id])
+   end
+
+   def update
+    @kitten = Kitten.find(params[:id])
+    
+    if @kitten.update(kitten_params)
+      redirect_to kitten_path(@kitten), notice: "Kitten updated!"
+    else
+      render 'edit'
+      flash.now[:alert] = "You fucked it up"
+    end
+   end
+
+   def destroy
+   end
+
+   private
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :softness, :cuteness)
+    end
+end
